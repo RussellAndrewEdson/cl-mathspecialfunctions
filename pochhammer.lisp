35da886e68e5cfa65caec41f42e2d10e317b3545
RussellAndrewEdson <russell.andrew.edson@gmail.com>
2015-09-25 14:17:43 +0930

    Coded up the Pochhammer symbol function.

--- pochhammer.lisp
+++ pochhammer.lisp
@@ -18,18 +18,35 @@
 ;;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ;;;; DEALINGS IN THE SOFTWARE.
 
-(in-package :cl-user)
 
-(defpackage :cl-mathspecialfunctions
-  (:use :common-lisp)
-  (:export :gamma
-           :nemes-approximation
-           :spouge-approximation
-           :windschitl-approximation
+;;;; Code for approximations of the Pochhammer symbol (rising factorial).
+;;;; Date: 25/09/2015
 
-           :beta
-           :beta-gamma-identity
-           :stirling-approximation
+(in-package :cl-mathspecialfunctions)
 
-           :pochhammer
-           :pochhammer-gamma-identity))
+;;; The Pochhammer symbol (rising factorial version) can be defined simply
+;;; as a ratio of gamma functions:
+(defun pochhammer-gamma-identity (a n)
+  "Returns the Pochhammer symbol as a ratio of gamma functions."
+  (/ (gamma (+ a n))
+     (gamma a)))
+
+;;; The Pochhammer symbol (rising factorial version). Here we use
+;;; the gamma ratio definition for the computation.
+;;;
+;;; Code Usage Examples:
+;;;
+;;;   Computing pochhammer(3,4) = 360
+;;;   > (pochhammer 3 4)
+;;;   360.00092684735586d0
+;;;
+;;;   Computing pochhammer(17,17) = 4.150171972903141d23
+;;;   > (pochhammer 17 17)
+;;;   4.150178572068358d23
+;;;
+;;;   Computing pochhammer(-3,-4) = 1/840 = 0.0011904762
+;;;   > (pochhammer -3 -4)
+;;;   #C(0.001190473055379472d0 -0.0d0)
+(defun pochhammer (a n)
+  "Returns an approximation for the Pochhammer symbol (a)_n."
+  (pochhammer-gamma-identity a n))
